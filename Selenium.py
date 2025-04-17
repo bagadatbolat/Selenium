0e00ea83aac226c8b4ffe8c3705cd2d4a3411da9
Bagadat <bagadatbolat@gmail.com>
2025-04-17 08:30:00 +0500

    Added selenium script for 3 browsers

--- Selenium.py
+++ Selenium.py
@@ -1,12 +1,28 @@
+import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-options = webdriver.ChromeOptions() # создаем переменную option для дальнейшего переиспользования
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+# создаем переменную base_url для дальнейшего переиспользования
+base_url = "https://www.saucedemo.com/"
 
-base_url = "https://www.saucedemo.com/" # создаем переменную base_url для дальнейшего переиспользования
-driver.get(base_url) # передаем переменную base_url драйверу, чтобы он смог открыть ссылку
-driver.maximize_window() # задаем разрешение,с которой драйвер откроет окно веб-браузера
+# Открытие сайта в Google Chrome
+chrome_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+chrome_driver.get(base_url)
+chrome_driver.maximize_window() # задаем разрешение,с которой драйвер откроет окно веб-браузера
+
+# Открытие сайта в Mozilla Firefox
+firefox_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+firefox_driver.get(base_url)
+firefox_driver.maximize_window() # задаем разрешение,с которой драйвер откроет окно веб-браузера
+
+# Открытие сайта в Microsoft Edge
+edge_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+edge_driver.get(base_url)
+edge_driver.maximize_window() # задаем разрешение,с которой драйвер откроет окно веб-браузера
